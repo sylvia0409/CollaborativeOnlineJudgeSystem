5fa9d309a5d567035d84a2dadadc5b1b22311015
sylvia0409 <sylvia0409siyu@gmail.com>
2018-02-19 22:32:12 -0500

    connect to mongoDB

--- oj-server/server.js
+++ oj-server/server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://admin:admin@ds241668.mlab.com:41668/problems');
 
 var restRouter = require('./routes/rest.js');
 
 app.use('/api/v1', restRouter);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, function() {
+     console.log('Example app listening on port 3000!')
+    })
--- oj-server/services/problemService.js
+++ oj-server/services/problemService.js
@@ -1,41 +1,49 @@
-var problems = [
-    {
-        id: 1,
-        name: "two sum",
-        desc: "101010",
-        difficulty: "easy"
-      },
-      {
-        id: 2,
-        name: "two sum2",
-        desc: "1010102",
-        difficulty: "hard"
-      },
-      {
-        id: 3,
-        name: "happy",
-        desc: "new year",
-        difficulty: "medium"
-      },
-      {
-        id: 4,
-        name: "welcome",
-        desc: "hahahaha",
-        difficulty: "super"
-      }
-]
+var problemModel = require('../models/problemModel');
+
 var getProblems = function(){
     return new Promise((resolve, reject) => {
-        resolve(problems);
+        problemModel.find({}, function(err, res) {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        })
     })
 }
 
-var getProblem = function(id){
+var getProblem = function(idNumber){
     return new Promise((resolve, reject) => {
-        resolve(problems.find(problem => problem.id === id));
+        problemModel.findOne({id: idNumber}, function(err, res){
+            if(err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        })
+    })
+}
+
+var addProblem = function(newProblem){
+    return new Promise((resolve,reject) => {       
+        problemModel.findOne({name: newProblem.name}, function(err, data){
+            if(data) {
+                reject('problem exist');                
+            } else {
+                problemModel.count({}, function(err, count){
+                    newProblem.id = count + 1;
+                    var mongoProblem = new problemModel(newProblem);
+                    mongoProblem.save();
+                    resolve(mongoProblem);
+                })
+                
+            }
+        })
     })
 }
 
 module.exports = {
-    getProblems
+    getProblems,
+    getProblem,
+    addProblem
 }
